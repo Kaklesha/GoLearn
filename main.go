eded2b106a223b8bdf3e6ab98499daf9b4eea16b
Kaklesha <k-korni1ov@yandex.ru>
2023-09-26 18:03:09 +0400

    done: 1 task

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+var someNamw="heel"
+
+func main() {
+   // fmt.Println(222)
+
+   //strings
+   //  var nameOne string = "ff"
+
+   //  var nameTwo = "luigu"
+
+   //  var nameThree string 
+
+    
+   // fmt.Println(nameOne,nameTwo,nameThree)
+
+   // nameOne = "fffas"
+  
+   // nameFour := "peach"
+
+   // fmt.Println(nameOne,nameFour)
+
+   //ints 
+
+   var ageOne int = 10
+   var ageTwo  = 20
+   ageTheee :=40
+   fmt.Println(ageOne,ageTwo,ageTheee)
+
+   // var numOne int8 = 25
+   // var numTwo int8 = -128
+   // var numThree uint16 = 256
+   // fmt.Println(ageOne,ageTwo,ageTheee,ageTrwo)
+
+   var scoreOne float32 = 25.98
+   var scoreTwe float64 = 888999898989889899889.7
+ 
+
+   scoreThree := 1.5
+
+}
