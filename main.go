611e834cab368c99baa4cfb72426b610aecc9d6f
Kaklesha <k-korni1ov@yandex.ru>
2023-09-26 18:39:32 +0400

    done5: arrays slice

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  // var ages [3] int = [3]int{20,25, 30} 
+var ages  = [3]int{20,25, 30} 
+
+names:= [4]string{"fff","gggg","vvvvv","bbbbb"}
+
+fmt.Println(ages, len(ages))
+fmt.Println(names, len(names))
+
+// slice (use arrays under the hood)
+var scores = []int{100,50,60}
+
+fmt.Println(scores, len(scores))
+
+scores[2]=25
+ scores = append(scores, 85)
+
+fmt.Println(scores, len(scores))
+
+//slice ranges
+
+rangeOne:=names[1:3]
+
+rangeTwo:=names[2:]
+
+rangeThree:=names[:3]
+
+
+fmt.Println(rangeOne,rangeTwo ,rangeThree)
+
+scoresh := append(rangeOne, "koopa")
+
+fmt.Println(scoresh,len(scores))
+
+
+}
