2c71094c5bd185b9fe3af0a760299e4f30a66c4d
Kaklesha <k-korni1ov@yandex.ru>
2023-10-01 22:42:44 +0400

    init: study proj

--- main.go
+++ main.go
@@ -9,11 +9,31 @@ var score = 99.5
 
 func main() {
 
-  sayHello("mario")
+ menu:= map[string] float64{
+   "soup": 4.99,
+   "pie": 7.99,
+   "salad": 6.99,
+   "coffee pudding": 3.55,
+ }
 
-  for _, v := range points {
-    fmt.Println(v)
+ fmt.Println(menu) 
+ fmt.Println("\n",menu["pie"])
+
+  for k, v := range menu {
+    fmt.Println(k," - ",v)
   }
 
-  showScore()
+  // ints as key type
+  phonebook := map[int]string{
+    23223323: "mario", 
+    897887897: "luigig", 
+    34342342: "peache",
+  }
+
+  fmt.Println(phonebook) 
+  fmt.Println("\n",phonebook[23223323])
+ 
+  phonebook[897887897]= "bpwser"
+
+  fmt.Println(phonebook) 
 }
