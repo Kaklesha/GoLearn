b08f5278e3df36d79f6d72b76ccff3c22e0ac374
Kaklesha <k-korni1ov@yandex.ru>
2023-10-01 22:00:03 +0400

    done: finc simple

--- main.go
+++ main.go
@@ -1,39 +1,40 @@
 package main
 
-import ("fmt")
+import ("fmt"
+"math"
+)
+
+func sayGreeting( n string )  {
+  fmt.Printf("Good morning %v  \n"  ,n)
+}
+
+func sayBye( n string )  {
+  fmt.Printf("Good bye %v  \n"  ,n)
+}
+
+func cycleNames( n [] string , f func(string))  {
+  for _,value := range n {
+    f(value)
+  }
+}
+
+func circleArea(r float64 ) float64 {
+  return math.Pi * r * r
+}
 
 func main() {
 
-  age:= 45 
-  fmt.Println(age <=50)
-  fmt.Println(age >=50)
+ 
 
-  fmt.Println(age ==50)
+ cycleNames([]string{"mario","luigi", "yoshi", "peache"}, sayGreeting)
 
-  fmt.Println(age !=50)
+ cycleNames([]string{"mario","luigi", "yoshi", "peache"}, sayBye)
+//names := [] string {"mario","luigi", "yoshi", "peache"}
 
+a1:=circleArea(4.2)
+a2:=circleArea(4)
 
-  if age < 30 {
-    fmt.Println("age is less 30")
-  } else if age < 40 {
-    fmt.Println("age is less than 40")
-  } else {
-    fmt.Println("age is not less than 45 ")
-  }
-
-names := [] string {"mario","luigi", "yoshi", "peache", "peache", "peache"}
-
-  for index, value := range names {
-    if index == 1 {
-      fmt.Println("continuinh at pos", index)
-      continue
-    }
-
-    if index >2 {
-      fmt.Println("breaking at pos", index)
-      break
-    }
-    fmt.Printf("the calue at pos %v is %v \n",index ,value)
-  }
+fmt.Println( a1,a2 )
+fmt.Printf( "circle 1 is %0.3f and circel 2 is %0.3f",a1,a2 )
 
 }
