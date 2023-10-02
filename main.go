1e5d8648c8155de4cc1d2ca75c19b0f9c974b484
Kaklesha <k-korni1ov@yandex.ru>
2023-10-02 14:00:03 +0400

    done: struct

--- main.go
+++ main.go
@@ -1,29 +1,11 @@
 package main
 
-import (
-  "fmt"
+import ("fmt")
 
-)
-
-//var score = 99.5
-
-func updateName (x *string){
-  *x="wedge"
-}
 
 func main() {
-name:="tifa"
- 
-m:=&name
 
- fmt.Println("memory address oa mane is: ",&name)
-fmt.Println(name)
+  mybill:= newBill("mario's bill")
+  fmt.Println(mybill)
 
-fmt.Println("memory address oa mane is: ",*m)
-*m="dddd"
-fmt.Println("memory address oa msse is: ",name)
-
-
-updateName(m)
-fmt.Println("memory address oa mane is: ",*m)
 }
