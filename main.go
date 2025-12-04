e5ed3fbf20a6cff0dc036b336d77068fab37c4e0
Kira <k-korni1ov@yandex.ru>
2025-12-04 12:16:18 +0400

    feat: arrays

--- main.go
+++ main.go
@@ -25,6 +25,16 @@ type Vertex struct {
 }
 
 func main() {
+	//Array
+	var a [2]string
+	a[0] = "hello"
+	a[1] = "World!"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+
 	//Struct Literals
 	var (
 		v1 = Vertex{1, 2}
