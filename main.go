7fbc489f2749b1462b8aa67b941461420d47ddae
Kira <k-korni1ov@yandex.ru>
2025-12-04 11:55:35 +0400

    feat: Pointers and dereferencing or indirecting

--- main.go
+++ main.go
@@ -2,40 +2,57 @@ package main
 
 import "fmt"
 
+func pointerViewer() {
+	var pp *int
+	fmt.Println(pp)
+	i, j := 42, 2701
+	p := &i //point to i
+	fmt.Println(p)
+	fmt.Println("----")
+	fmt.Println(*p) //read i through the pointer
+	*p = 21         // set i through the pointer
+	fmt.Println(i)  //see the new value of i
+	//dereferencing OR indirecting !!!!
+	p = &j       //point to j
+	*p = *p / 37 //devide j through the pointer
+	fmt.Println(j)
+
+}
+
 func main() {
+	//Pointers AND 	//dereferencing OR indirecting !!!!
+	pointerViewer()
+	fmt.Println("====================================")
+	// var ages [3] int = [3]int{20,25, 30}
+	//	var ages = [3]int{20, 25, 30}
 
-  // var ages [3] int = [3]int{20,25, 30} 
-var ages  = [3]int{20,25, 30} 
+	//	names := [4]string{"fff", "gggg", "vvvvv", "bbbbb"}
 
-names:= [4]string{"fff","gggg","vvvvv","bbbbb"}
+	//fmt.Println(ages, len(ages))
+	//fmt.Println(names, len(names))
 
-fmt.Println(ages, len(ages))
-fmt.Println(names, len(names))
+	// //slice (use arrays under the hood)
+	//var scores = []int{100, 50, 60}
 
-// slice (use arrays under the hood)
-var scores = []int{100,50,60}
+	//	fmt.Println(scores, len(scores))
 
-fmt.Println(scores, len(scores))
+	//scores[2] = 25
+	//scores = append(scores, 85)
 
-scores[2]=25
- scores = append(scores, 85)
+	//fmt.Println(scores, len(scores))
 
-fmt.Println(scores, len(scores))
+	////slice ranges
 
-//slice ranges
+	//rangeOne := names[1:3]
 
-rangeOne:=names[1:3]
+	//rangeTwo := names[2:]
 
-rangeTwo:=names[2:]
+	//rangeThree := names[:3]
 
-rangeThree:=names[:3]
+	//fmt.Println(rangeOne, rangeTwo, rangeThree)
 
+	//scoresh := append(rangeOne, "koopa")
 
-fmt.Println(rangeOne,rangeTwo ,rangeThree)
-
-scoresh := append(rangeOne, "koopa")
-
-fmt.Println(scoresh,len(scores))
-
+	//fmt.Println(scoresh, len(scores))
 
 }
